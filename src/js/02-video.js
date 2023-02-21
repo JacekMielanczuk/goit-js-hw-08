81262433bd34f2db35733d0144fe152305c59e8a
JacekMielanczuk <mielanczuk1988@gmail.com>
2023-02-21 18:36:31 +0100

    podejście 1
    podejście 1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import '../css/common.css';
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const TIME_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const onPlay = function (data) {
+  const stringfyData = JSON.stringify(data);
+  localStorage.setItem(TIME_KEY, stringfyData);
+};
+player.on('timeupdate', throttle(onPlay, 1000));
